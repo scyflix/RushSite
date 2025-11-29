a037e68e8f5538f67992e756ba4769b365389c2b
abdulroqibscybud <abdulroqeeboladipo@gmail.com>
2025-11-29 09:58:52 +0100

    add cancle icon on menu icon click and edit  menu link size

--- main/sidebar.js
+++ main/sidebar.js
@@ -1,5 +1,12 @@
 const menuIcon = document.querySelector(".menuIcon");
 const sidebarContainer = document.getElementById("sidebarContainer");
+const topbar = document.getElementById("topbar");
+const middlebar = document.getElementById("middlebar");
+const bottombar = document.getElementById("bottombar");
 menuIcon.addEventListener("click", () => {
   sidebarContainer.classList.toggle("show");
+  
+  topbar.classList.toggle("crossForward");
+  bottombar.classList.toggle("crossBackward");
+  middlebar.classList.toggle("remove");
 });
