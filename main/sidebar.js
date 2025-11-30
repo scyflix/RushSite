c9d1c0828d30d320d3e0be7da303a28928065a2f
abdulroqibscybud <abdulroqeeboladipo@gmail.com>
2025-11-30 19:15:45 +0100

    make overlay close sidebar on click and change sidebar logic to tenery operator

--- main/sidebar.js
+++ main/sidebar.js
@@ -3,10 +3,23 @@ const sidebarContainer = document.getElementById("sidebarContainer");
 const topbar = document.getElementById("topbar");
 const middlebar = document.getElementById("middlebar");
 const bottombar = document.getElementById("bottombar");
+function cancelIcon() {
+  topbar.classList.add("crossForward");
+  bottombar.classList.add("crossBackward");
+  middlebar.classList.add("remove");
+}
+function cancelIconRemove() {
+  topbar.classList.remove("crossForward");
+  bottombar.classList.remove("crossBackward");
+  middlebar.classList.remove("remove");
+}
 menuIcon.addEventListener("click", () => {
-  sidebarContainer.classList.toggle("show");
-  
-  topbar.classList.toggle("crossForward");
-  bottombar.classList.toggle("crossBackward");
-  middlebar.classList.toggle("remove");
+  const open = sidebarContainer.classList.toggle("show");
+  open ? cancelIcon() : cancelIconRemove();
+});
+sidebarContainer.addEventListener("click", (e) => {
+  if (e.target === sidebarContainer) {
+    sidebarContainer.classList.remove("show");
+    cancelIconRemove();
+  }
 });
