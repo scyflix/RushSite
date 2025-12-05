45d0bb7740b40fb6443796f2ba382b38cd0e2d67
abdulroqibscybud <abdulroqeeboladipo@gmail.com>
2025-12-05 20:57:03 +0100

    Change default theme to darkmode and edit some spacing while also adopt CSS variables

--- main/sidebar.js
+++ main/sidebar.js
@@ -3,20 +3,29 @@ const sidebarContainer = document.getElementById("sidebarContainer");
 const topbar = document.getElementById("topbar");
 const middlebar = document.getElementById("middlebar");
 const bottombar = document.getElementById("bottombar");
+
+
+//Sidebar cancel icon
 function cancelIcon() {
   topbar.classList.add("crossForward");
   bottombar.classList.add("crossBackward");
   middlebar.classList.add("remove");
 }
+
+//sidebar cancel icon remove
 function cancelIconRemove() {
   topbar.classList.remove("crossForward");
   bottombar.classList.remove("crossBackward");
   middlebar.classList.remove("remove");
 }
+
+//sidebar toggle
 menuIcon.addEventListener("click", () => {
   const open = sidebarContainer.classList.toggle("show");
   open ? cancelIcon() : cancelIconRemove();
 });
+
+//close sidebar when clciking
 sidebarContainer.addEventListener("click", (e) => {
   if (e.target === sidebarContainer) {
     sidebarContainer.classList.remove("show");
