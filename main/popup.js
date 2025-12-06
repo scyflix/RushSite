2ea1e3035151b4e8efe1749066202029857dae32
abdulroqibscybud <abdulroqeeboladipo@gmail.com>
2025-12-06 14:21:32 +0100

    refactor: enhance popup functionality and improve UI elements

--- main/popup.js
+++ main/popup.js
@@ -1,9 +1,18 @@
 const PopupContainer = document.getElementById("PopupContainer");
 const cancelBtn = document.getElementById("cancelPopup");
+const body = document.body;
 
+if(!PopupContainer.classList.contains("cancel")) {
+body.classList.add("popup-active");
+}
+// Function to close the popup
+function closePopup() {
+   PopupContainer.classList.add("cancel");
+   setTimeout(() => {
+     PopupContainer.style.display = "none";
+   }, 2000);
+   body.classList.remove("popup-active");
+  }
 cancelBtn.addEventListener("click", () => {
-  PopupContainer.classList.add("cancel");
-  setTimeout(() => {
-    PopupContainer.style.display = "none";
-  }, 2000);
+ closePopup();
 });
